7ce0d7783a1051ec59366b1db4377a669558a934
turchak <turchaksv@gmail.com>
2018-01-08 22:09:28 +0200

    add current weather

--- app.js
+++ app.js
@@ -1,25 +1,115 @@
 const secretKey = '74e2fcaef808ae167e9dedf219a7e6a0';
-const input = document.querySelector('.search');
-const button = document.querySelector('.search-button');
+const units = 'si';
+
+
+const searchInput = document.querySelector('.search-input');
+const searchButton = document.querySelector('.search-button');
+const cityName = document.querySelector('.city-name');
+
+const current = {
+    icon:        document.querySelector('.icon'),
+    summary:     document.querySelector('.current-summary'),
+    wind:        document.querySelector('.current-wind-num'),
+    temperature: document.querySelector('.current-temp'),
+    humidity:    document.querySelector('.current-humidity-num'),
+    pressure:    document.querySelector('.current-pressure-num')
+}
 
 let coordinates = [];
 
 function getData() {
     let latitude = coordinates[0].toString();
     let longitude = coordinates[1].toString();
-    let options = {
-        // mode : 'no-cors'
-    };
-    let url = "https://api.darksky.net/forecast/" + secretKey + "/" + latitude + "," + longitude;
-
-    fetch(url, options)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        })
+    let url = "https://api.darksky.net/forecast/" + secretKey + "/" + latitude + "," + longitude + "?units=" + units;
+    $.ajax({
+        url: url,
+        dataType: "jsonp"
+    }).done(function (data) {
+        let currently = data.currently;
+        renderData(currently);
+        console.log(data);
+    });
     coordinates.length = 0;
+}
+
+function getDate(data) {
+    let date = new Date(data.time * 1000);
+    let monthArr = ['Junuary', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let month = date.getMonth();
+    console.log(monthArr[month]);
+    console.log(date.getFullYear());
+    console.log(date.getDate());
+}
+
+
+class Weather {
+    constructor(entry) {
+        this.icon = entry.icon;
+        this.summary = entry.summary;
+        this.wind = entry.wind;
+        this.temperature = entry.temperature;
+        this.humidity = entry.humidity;
+        this.pressure = entry.pressure;
+    }
+
+    getIcon(data) {
+        this.icon.className = 'icon wi';
+        let dataIcon = data.icon;
+        switch (dataIcon) {
+            case "partly-cloudy-night" : this.icon.classList.add("wi-night-alt-cloudy");
+                break;
+
+            case "clear-night" : this.icon.classList.add("wi-night-clear");
+                break;
+
+            case "cloudy" : this.icon.classList.add("wi-cloud");
+                break;
+
+            case "clear-day" : this.icon.classList.add("wi-day-sunny");
+                break;
+
+            case "rain" : this.icon.classList.add("wi-rain");
+                break;
+
+            case "partly-cloudy-day" : this.icon.classList.add("wi-cloudy");
+                break;
+            case "snow" : this.icon.classList.add("wi-snow");
+        }
+    }
+
+    getSummary(data) {
+        this.summary.innerHTML = data.summary;
+    }
+
+    getWind(data) {
+        this.wind.innerHTML = Math.round(data.windSpeed);
+    }
+
+    getTemperature(data) {
+        this.temperature.innerHTML = Math.round(data.temperature);
+    }
+
+    getHumidity(data) {
+        this.humidity.innerHTML = Math.round(data.humidity);
+    }
+
+    getPressure(data) {
+        this.pressure.innerHTML = Math.round(data.pressure);
+    }
+
+}
+
+
+let currentWeather = new Weather(current);
+
+
+function renderData(data) {
+    currentWeather.getSummary(data);
+    currentWeather.getIcon(data);
+    currentWeather.getTemperature(data);
+    currentWeather.getWind(data);
+    currentWeather.getHumidity(data);
+    currentWeather.getPressure(data);
 }
 
 function getCoordinates(city) {
@@ -28,6 +118,9 @@ function getCoordinates(city) {
         if (status == google.maps.GeocoderStatus.OK) {
             let lat = results[0].geometry.location.lat();
             let lng = results[0].geometry.location.lng();
+            let name = results[0].address_components[0].long_name;
+            console.log(results[0].address_components[0]);
+            cityName.innerHTML = name;
             coordinates.push(lat, lng);
             console.log("location : " + results[0].geometry.location.lat() + " " + results[0].geometry.location.lng());
             getData();
@@ -37,8 +130,8 @@ function getCoordinates(city) {
     });
 }
 
-button.addEventListener('click', () => {
-    let inputValue = input.value;
+searchButton.addEventListener('click', () => {
+    let inputValue = searchInput.value;
     getCoordinates(inputValue);
 })
 
